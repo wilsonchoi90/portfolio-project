1c2a6b560929c4c6389d22f9ce046c833d0d1ac2
wilsonchoi <wilson.choi90@gmail.com>
2023-05-08 02:09:54 -0400

    updated headshot, added animations to section content, created script for load on scroll effect

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ window.onbeforeunload = () => {
         form.reset();
     }
 }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slide-in-bottom');
+        }
+    })
+})
+observer.observe(document.querySelector('.hero-parent'));
+observer.observe(document.querySelector('.about-parent'));
+observer.observe(document.querySelector('.skills-parent'));
+observer.observe(document.querySelector('.portfolio-parent'));
+observer.observe(document.querySelector('.additional-parent'));
+observer.observe(document.querySelector('.contact-parent'));
